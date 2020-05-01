da27ecfcb2deb994e3d95f8c44572b92e5d7d9ec
Gaspar <gabriel.sgaspar@yahoo.com.br>
2020-05-01 16:38:36 -0300

    implement chat between clients using sockets

--- server.js
+++ server.js
@@ -21,10 +21,23 @@ let messages = [];
 // Configura conexão dos clientes através do socket
 io.on('connection', socket => {
     console.log(`Socket conectado: ${socket.id}`);
+    socket.emit('contentChat', messages);
+    
     socket.on('sendMessage', data => {
-        console.log(data);
         messages.push(data);
+        socket.broadcast.emit("receivedMessage", data);
     });
+   
 });
 
 server.listen(3000);
+
+
+/*
+    O socket tem três eventos basicamente:
+    socket.on() -> Usado para ouvir a chegada de mensagens pelo socket. Cada mensagem que chega possui uma identificação. Então você pode configurar diferentes lógicas para diferentes tipos de mensagens que chegam pelo socket.
+
+    socket.emit() -> Envia uma mensagem via socket do servidor para o cliente, através do socket que conecta ambos
+
+    socket.broadcast.emit() -> Envia uma mensagem para todos os sockets com os quais o servidor possui conexão. Essa mensagem também recebe uma identificação, e assim, no lado do cliente, você programa o socket para ouvir por notificações dos tipos identificados.
+*/
